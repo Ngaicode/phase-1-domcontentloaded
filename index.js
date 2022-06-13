50f24aa98470d4ab05d3b85bbabbd27a603ae028
Ngaicode <mjombakevin376@gmail.com>
2022-06-13 23:10:30 +0300

    commit tree 71eda88db91c425cd4a83f333db813e0c44e14f9

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+"use strict";
+// this listens for the DOMContentLoaded event on the DOM
+const domLoaded = document.addEventListener("DOMContentLoaded", function () {
+  domUpdater();
+});
+// this is the event handler
+function domUpdater() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
